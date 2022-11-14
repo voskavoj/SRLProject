483fec14b551136ca7bd422718f5451b8e7652b6
Voska Vojtech <vojtech.voska@comap-control.com>
2022-11-14 15:12:26 +0100

    Extract tuning coefficients

--- tuning.m
+++ tuning.m
@@ -1,30 +1,38 @@
 %% LATERAL CONTROL
 
 % Roll Control
-RollCtrl.P = 1;
-RollCtrl.I = 0;
-RollCtrl.D = 0;
-RollCtrl.damping = 0;
+RollCtrl.P = 1.5;
+RollCtrl.I = 0.15;
+RollCtrl.damping = 0.2;
+RollCtrl.saturation = 50;
+
+% Heading control
+HeadingCtrl.P = 200/30;
+HeadingCtrl.I = 145/30;
+HeadingCtrl.D = 69/30;
+HeadingCtrl.rate = 0.05;
 
 %% LONGITUDINAL CONTROL
 % Pitch Control
-PitchCtrl.P = 1.0224393073205*0.9;
-PitchCtrl.I = 0.0357890292942185*4;
-PitchCtrl.D = 0;
-PitchCtrl.saturation.upper = deg2rad(10);
-PitchCtrl.saturation.lower = deg2rad(-15);
+PitchCtrl.P = 2;
+PitchCtrl.I = 0.1;
+PitchCtrl.D = 0.1;
+PitchCtrl.saturation.upper = 10;
+PitchCtrl.saturation.lower = -15;
 PitchCtrl.damping = 0.2;
 
 % Altitude Control
-AltCtrl.P = 0;
-AltCtrl.I = 0;
-AltCtrl.D = 0;
-AltCtrl.damping = 0;
+AltCtrl.P = 0.009;
+AltCtrl.I = 0.000011;
+AltCtrl.D = 0.0002;
+AltCtrl.rate.rising = 3;
+AltCtrl.rate.falling = -10;
 
 % Airspeed Control
-AirspdCtrl.P = 0;
-AirspdCtrl.I = 0;
-AirspdCtrl.D = 0;
+AirspdCtrl.P = 100;
+AirspdCtrl.I = 10;
 AirspdCtrl.double_integration = 0;
+AirspdCtrl.saturation.upper = 40;
+AirspdCtrl.saturation.lower = -30;
 
 fprintf("Control coefficients loaded.\n")
