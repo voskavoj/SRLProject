37a640b1c8335e65c5e8651dc7a01ec7cd411bb0
Voska Vojtech <vojtech.voska@comap-control.com>
2023-01-23 16:38:46 +0100

    Double integration to HeadingCtrl

--- tuning.m
+++ tuning.m
@@ -8,10 +8,14 @@ RollCtrl.damping = 0.2;
 RollCtrl.saturation = 50;
 
 % Heading control
-HeadingCtrl.P = 200/30;
-HeadingCtrl.I = 145/30;
-HeadingCtrl.D = 69/30;
+HeadingCtrl.errPID.P = 200/30;
+HeadingCtrl.errPID.I = 145/30;
+HeadingCtrl.errPID.D = 69/30;
+HeadingCtrl.accPID.P = 3;
+HeadingCtrl.accPID.I = 0;
+HeadingCtrl.accPID.D = 0.04;
 HeadingCtrl.rate = 0.05;
+HeadingCtrl.filter.fc = 2;
 
 %% LONGITUDINAL CONTROL
 % Pitch Control
@@ -25,12 +29,10 @@ PitchCtrl.saturation.lower = -15;
 AltCtrl.errPID.P = 0.5;
 AltCtrl.errPID.I = 0.005;
 AltCtrl.errPID.D = 0.0;
-AltCtrl.accPID.P = 0.06;
-AltCtrl.accPID.I = 0.005;
-AltCtrl.accPID.D = 0.01;
+
 AltCtrl.rate.rising = 3;
 AltCtrl.rate.falling = -10;
-AltCtrl.filter.fc = 20;
+
 
 % Airspeed Control
 AirspdCtrl.P = 100;
